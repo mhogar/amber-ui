5714d6c63d81a2f54242207abce92722b9ddf64c
Ryan <mhogar98@gmail.com>
2021-10-04 21:13:37 -0400

    Add bootstrap styling + setup routing + add page stubs

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,55 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="wrapper">
+    <nav class="navbar navbar-expand-md navbar-light fixed-top">
+      <div class="container-fluid">
+        <router-link to="/" class="navbar-brand">amber</router-link>
+        <button class="navbar-toggler" type="button"
+                data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav me-auto mb-2 mb-md-0">
+            <li class="nav-item">
+              <router-link to="/users" class="nav-link">Users</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/clients" class="nav-link">Clients</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <router-view v-slot="{ Component }">
+      <transition enter-active-class="animate__animated animate__faster animate__fadeIn"
+                  leave-active-class="animate__animated animate__faster animate__fadeOut"
+                  mode="out-in">
+        <component id="currentPage" :is="Component" />
+      </transition>
+    </router-view>
   </div>
-  <router-view/>
 </template>
 
-<style>
+<style lang="scss">
+
+@import "./assets/theme.scss";
+@import "../node_modules/bootstrap/scss/bootstrap";
+
+body {
+  background-color: $page;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  padding-top: 4.5rem;
 }
 
-#nav {
-  padding: 30px;
+.wrapper {
+  max-width: 1680px;
+  margin-left: auto;
+  margin-right: auto;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.navbar {
+  background-color: $navbar;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -8,13 +8,15 @@ const routes: Array<RouteRecordRaw> = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/users',
+    name: 'Users',
+    component: () => import('../views/Users.vue')
+  },
+  {
+    path: '/clients',
+    name: 'Clients',
+    component: () => import('../views/Clients.vue')
+  },
 ]
 
 const router = createRouter({
